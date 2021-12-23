1633cf4ad6242a494bc56a219e4f2e4eb7285a4b
Wouter Loeve <wouterloeve0@gmail.com>
2021-12-23 15:44:27 +0100

    Added support for describing multiple units in the configuration file and cleaned up some mess. Still untested though

--- main.py
+++ main.py
@@ -12,7 +12,25 @@ extended_delay = 10 * long_delay
 NUM_ATTEMPTS = 20
 TRANSMIT_PIN = 18
 
+def create_kaku_message(name, group, state, unit):
+    """
+    Creates a bit string (string not bit array) message.
+    Primary source: https://www.circuitsonline.net/forum/view/message/1181410
+
+    :param name: the code associated with the remote. You'll have to extract this from the signal, unless you have a configurable one. We used: https://www.instructables.com/Super-Simple-Raspberry-Pi-433MHz-Home-Automation/
+    :param group: group id, can have multiple devices. We don't use this.
+    :param state: 1 for on, 0 for off.
+    :param unit: corresponds with the seperate buttons on the remote, this can be used to add multiple receivers
+        Also note that it's possible to assign multiple unit codes to the same receiver
+    
+    :returns: String that can be used by transmit_code_new.
+    """
+    return "B{name:026b}{group}{state}{unit:04b}E".format(name, group, state, unit)
+
 def gpio_setup():
+    """
+    Sets up GPIO for sending messages.
+    """
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(TRANSMIT_PIN, GPIO.OUT)
 
@@ -77,11 +95,10 @@ if __name__ == '__main__':
     if (len(sys.argv) > 1):
         state = sys.argv[1]
 
-    # Main source: https://www.circuitsonline.net/forum/view/message/1181410
-    # name is the code associated with the remote. You'll have to extract this from the signal, unless you have a configurable one.
-    # We used: https://www.instructables.com/Super-Simple-Raspberry-Pi-433MHz-Home-Automation/
-    # group is the group id, can have multiple devices. We don't use this.
-    # unit corresponds with the seperate buttons on the remote, this can be used to add multiple receivers
-    # Also note that it's possible to assign multiple unit codes to the same receiver
-    argument = "B{name:026b}{group}{state}{unit:04b}E".format(name=config["name"], group=config["group"], state=state, unit=config["unit"])
-    transmit_code_new(str(argument))
+    if isinstance(config["unit"], list):
+        for unit in config["unit"]:
+            code = create_kaku_message(config["name"], config["group"], state, config["unit"][unit])
+            transmit_code_new(code)
+    else:    
+        code = create_kaku_message(config["name"], config["group"], state, config["unit"])
+        transmit_code_new(code)
